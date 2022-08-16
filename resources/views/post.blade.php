4e3b23ee392abea067a3bf1ad69a7df0c0f9023f
Ali <asadri721@gmail.com>
2022-08-16 17:51:37 +0430

    Add eager loading

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
         <div>
             <h1>{{ $post->title }}</h1>
             <p>{{ $post->excerpt }}</p>
+            <p><a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a></p>
             <p><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
             <p>{{ $post->body }}</p>
         </div>

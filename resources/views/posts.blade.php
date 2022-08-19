62ae1c88bc147692e1684aca17dd98ff9a901a5b
Ali <asadri721@gmail.com>
2022-08-19 06:47:53 +0430

    Convert index page to designed blade page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,21 @@
 <x-layout>
-    <slot>
+    @include('_post-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+    {{-- <slot>
         @foreach($posts as $post)
             <div>
                 By <a href="/authors/{{$post->author->username}}">{{ $post->author->name }}</a><a href="/posts/{{ $post->id }}"><h2>{{ $post->title }}</h2></a>
@@ -10,5 +26,5 @@
             </div>
             <hr>
         @endforeach
-    </slot>
+    </slot> --}}
 </x-layout>

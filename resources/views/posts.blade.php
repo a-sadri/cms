9ec175f2a31380066cfdc046382228a7943ccbdd
Ali <asadri721@gmail.com>
2022-08-20 07:43:20 +0430

    Add posts grid

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,29 +2,13 @@
     @include('_post-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
 
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
     </main>
-    {{-- <slot>
-        @foreach($posts as $post)
-            <div>
-                By <a href="/authors/{{$post->author->username}}">{{ $post->author->name }}</a><a href="/posts/{{ $post->id }}"><h2>{{ $post->title }}</h2></a>
 
-                <p><a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
-
-                {{ $post->excerpt }}
-            </div>
-            <hr>
-        @endforeach
-    </slot> --}}
 </x-layout>
